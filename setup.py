88487b1b3dc2bf0fd9c04dda5fd9f8f9823b6c2d
Ryan Strauss <ryanrstrauss@icloud.com>
2021-11-11 20:30:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 from setuptools import setup
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
 setup(
     name="bax",
     version="0.1",
     packages=["bax"],
     url="https://github.com/rystrauss/bax",
-    license="",
+    license="LICENSE",
     author="Ryan Strauss",
     author_email="ryanrstrauss@icloud.com",
     description="A flexible trainer interface for Jax and Haiku.",
     python_requires=">=3.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         "dm-haiku>=0.0.5",
         "jax>=0.2.24",
         "optax>=0.0.9",
         "chex>=0.0.8",
         "keras>=2.6.0",
-        "tensorflow>=2.6"
+        "tensorflow>=2.6",
     ],
 )
